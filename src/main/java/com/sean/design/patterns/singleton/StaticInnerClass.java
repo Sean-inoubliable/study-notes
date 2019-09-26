c3b3a55dbae76670aba9e77731070bec98064b14
Sean-inoubliable <inoubliable_s@163.com>
2019-09-26 09:01:17 +0800

    设计模式-单例模式-静态内部类

---
+++ src/main/java/com/sean/design/patterns/singleton/StaticInnerClass.java
@@ -0,0 +1,38 @@
+package com.sean.design.patterns.singleton;
+
+/**
+ * @Description: 设计模式 - 单例模式 - 静态内部类（JVM类装载机制）
+ * @Author: Sean
+ * @Date: 2019/9/23 0023 18:59
+ */
+public class StaticInnerClass {
+    public static void main(String[] args) {
+        SingletonStaticInnerClass instance = SingletonStaticInnerClass.getInstance();
+        SingletonStaticInnerClass instance1 = SingletonStaticInnerClass.getInstance();
+        System.out.println(instance == instance1);
+        System.out.println(instance.equals(instance1));
+        System.out.println(instance.hashCode());
+        System.out.println(instance1.hashCode());
+    }
+}
+
+/**
+ * 1. 外部类装在的时候，静态内部类并不会立即被装载
+ * 2. 在调用getInstance()方法时，会致使静态内部类的装载，而且只会装载一次
+ * 3. JVM在装载类时，线程是安全的
+ */
+class SingletonStaticInnerClass {
+
+    private SingletonStaticInnerClass() {}
+
+    /**
+     * 静态内部类，该类中有一个静态属性
+     */
+    private static class SingletonStaticInnerClassInstance {
+        private static final SingletonStaticInnerClass INSTANCE = new SingletonStaticInnerClass();
+    }
+
+    public static SingletonStaticInnerClass getInstance() {
+        return SingletonStaticInnerClassInstance.INSTANCE;
+    }
+}
