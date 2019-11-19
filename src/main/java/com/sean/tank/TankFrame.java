1cfa74f11406934f81c86ab0607af77231519934
Sean <songxinlei@csii.com.cn>
2019-11-19 16:40:36 +0800

    键盘监听事件

--- src/main/java/com/sean/tank/TankFrame.java
+++ src/main/java/com/sean/tank/TankFrame.java
@@ -1,6 +1,8 @@
 package com.sean.tank;
 
 import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
@@ -23,6 +25,9 @@ public class TankFrame extends Frame {
         //将窗口设为可见状态
         setVisible(true);
 
+        /* 内部类 */
+        this.addKeyListener(new MyKeyListener());
+
         /* 添加一个 Window的监听器, 监听 windowClosing
            用于窗口关闭
            传入一个匿名内部类 */
@@ -34,11 +39,28 @@ public class TankFrame extends Frame {
         });
     }
 
+    int x = 200, y=200;
+
     /* paint(Graphics graphics)窗口需要重新绘制的时候，将被系统自动调用
        Graphics可当做是一支画笔 */
     @Override
     public void paint(Graphics graphics) {
         //坐标 x，坐标 y，宽，长
-        graphics.fillRect(200, 200, 50, 50);
+        graphics.fillRect(x, y, 50, 50);
+    }
+
+    /* 键盘监听处理类，处理键盘事件 */
+    class MyKeyListener extends KeyAdapter {
+
+        //会在一个键按下的时候被调用
+        @Override
+        public void keyPressed(KeyEvent e) {
+            System.out.println("key pressed!");
+        }
+        //会在一个键抬起的时候被嗲用
+        @Override
+        public void keyReleased(KeyEvent e) {
+            System.out.println("key released！");
+        }
     }
 }
