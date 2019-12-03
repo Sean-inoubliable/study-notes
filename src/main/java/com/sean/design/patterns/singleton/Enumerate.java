6e123ab7610bdde35f14bd0b4895bdb0614308e2
Sean <songxinlei@csii.com.cn>
2019-12-03 15:03:45 +0800

    单例-枚举理解补充

--- src/main/java/com/sean/design/patterns/singleton/Enumerate.java
+++ src/main/java/com/sean/design/patterns/singleton/Enumerate.java
@@ -2,6 +2,13 @@ package com.sean.design.patterns.singleton;
 
 /**
  * @Description: 设计模式 - 单例模式 - 枚举
+ *
+ * 不仅可以解决线程同步，还可以防止反序列化
+ * Java的反射是可以通过一个.class文件，把整个Class load到内存，然后再把它 new一个实例出来
+ * 所以严格来讲，枚举以外的单例都是不安全的
+ *
+ * 枚举单例不会被反序列化的原因是：枚举类没有构造方法，所以即使拿到.class文件，也没有办法构造其对象
+ *
  * @Author: Sean
  * @Date: 2019/9/23 0023 19:21
  */
